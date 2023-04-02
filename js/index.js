66abf3315e2088264790a71826ec6e242915d1a9
3LaaElnaggar <elnaggar390@gmail.com>
2023-04-02 08:52:07 +0200

    using-animation

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
 let butripple=document.getElementById("butripple");
-let changpos=document.getElementById("changpos");
+// let changpos=document.getElementById("changpos");
 
-butripple.addEventListener("mouseenter",(e)=>{
+butripple.addEventListener("click",(e)=>{
 console.log(e);
-let x=e.pageX - butripple.offsetLeft;
-let y=e.pageY - butripple.offsetTop;
+let changpos=document.createElement("div")
+changpos.classList="mobripp active";
+// changpos.classList="active";
+butripple.appendChild(changpos)
+let x=e.clientX - butripple.offsetLeft;
+let y=e.clientY - butripple.offsetTop;
 changpos.style.left=x+"px";
 changpos.style.top=y+"px"; 
 
+setTimeout(()=>{
+  changpos.remove();
+},1000)
+
 
 });
+// butripple.addEventListener("mouseleave",(e)=>{
+
+
+
+// });
+// butripple.onmouseleave
